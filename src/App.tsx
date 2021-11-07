7a0b1d26d0efcd206ea20784f6cee5907313f34e
bert-fabillar <herbert@convrtx.com>
2021-11-07 16:53:41 +0800

    source code migration

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+ 
+import Home from './components/Home/home';
+import SignIn from './components/SignIn/signin';
+ 
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/signin" component={SignIn} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+ 
+export default App;

d028abbab4aa88b1a2d2a81c585d475ec5dd3a0e
Bernhard Paolo Cena <bcena@convrtx.com>
2022-03-30 17:40:39 +0800

    GUIDED-151: Added sub admin support page

--- src/components/pages/sub-admin/support/SubSupportScreen.tsx
+++ src/components/pages/sub-admin/support/SubSupportScreen.tsx
@@ -1,12 +1,33 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import "./SubSupport.scss";
+import InboxList from './InboxList';
+import MessageThread from './MessageThread';
 
 const SubSupportScreen = () => {
   return (
-    <Container>
+    <Container className="sub-support-container">
       <Row className="mt-5">
         <Col>
           <h2>Support</h2>
         </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col>
+          <h4>Reported To Admin</h4>
+        </Col>
+      </Row>
+      <Row className="mt-3 chat-list-content">
+        <div className="inline-block chat-list">
+          <div className='chat-list-header'>
+            <h5>Messages</h5>
+          </div>
+          <div className='nav'>
+            <button className='nav-buttons button-active'> All </button>
+            <button className='nav-buttons'> Archive </button>
+          </div>
+          <InboxList />
+        </div>
+       <MessageThread />
       </Row>
     </Container>
   );
